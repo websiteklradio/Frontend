98c1489bbcb5697ec88f072bcf146119685f0b1a
Meghavarshini Prathapani <meghavarshiniprathapani@gmail.com>
2025-12-10 13:19:25 +0000

    in footer section remove built for and give only KL Radio

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -9,7 +9,7 @@ export function SiteFooter() {
         <div className="flex items-center gap-2">
           <Image src="https://ik.imagekit.io/z5fowzj2wr/Screenshot%202025-12-10%20171402.png?updatedAt=1765367128945" alt="KL Radio Logo" width={20} height={20} className="h-5 w-5" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built for KL Radio. &copy; {new Date().getFullYear()}
+            KL Radio &copy; {new Date().getFullYear()}
           </p>
         </div>
         <div className="flex items-center gap-6">
