e6cc872a1d6f3152fc0c438137eb9602ee1aa412
Meghavarshini <meghavarshiniprathapani@gmail.com>
2026-01-25 14:26:29 +0530

    Change API base URL to production endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://radio-backend-1i7e.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
