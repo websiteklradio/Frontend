6d8db8d6fea5e5385f40d94cafa02abb6e258d4d
Meghavarshini Prathapani <meghavarshiniprathapani@gmail.com>
2026-02-05 16:44:39 +0000

    https://backend-mbht.onrender.com
    this is my new backend url update thi

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://radio-backend-2y3k.onrender.com/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://backend-mbht.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
