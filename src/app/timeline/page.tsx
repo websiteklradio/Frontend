0407d0d6d0dc88c5711953f887999eca7d4fa6a5
Meghavarshini Prathapani <meghavarshiniprathapani@gmail.com>
2026-02-06 06:57:35 +0000

    in card 15 add 2024-July under that add heading as Announcement of New H

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -145,9 +145,9 @@ const TimelinePage: React.FC = () => {
     },
     {
       id: '15',
-      date: '2027-July',
-      title: 'Summer Music Festival',
-      description: 'Hosted the first annual KL Radio Summer Music Festival, featuring student bands and local artists.',
+      date: '2024-July',
+      title: 'Announcement of New Heads',
+      description: '',
       icon: <Book className="text-white" />,
       image: 'https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
       category: 'Community',
